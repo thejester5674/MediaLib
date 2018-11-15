7fd16c99911e792f78d83e0057f979af721cfb9b
Josh West <thejester5674@gmail.com>
2018-11-15 13:17:33 -0600

    loop

--- LoopingMediaLib.java
+++ LoopingMediaLib.java
@@ -2,9 +2,15 @@ public class LoopingMediaLib
 {
    public static void main()
 {
+   String songInfo = MediaFile.readString();
    for(int i = 0; i < 3; i++)
    {
         System.out.println(MediaFile.readString());
+   }
+   while (songInfo != null)
+   {
+       System.out.println(songInfo);
+       songInfo = MediaFile.readString();
    }
 }
 }
